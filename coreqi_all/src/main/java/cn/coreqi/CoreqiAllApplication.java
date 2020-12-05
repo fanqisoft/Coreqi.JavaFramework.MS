d66403e93524d7079c4e529f7a6f7e8181009137
fanqi <fanqisoft@gmail.com>
2020-12-05 12:47:27 +0800

    初始化项目结构

---
+++ coreqi_all/src/main/java/cn/coreqi/CoreqiAllApplication.java
@@ -0,0 +1,13 @@
+package cn.coreqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreqiAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreqiAllApplication.class, args);
+    }
+
+}
